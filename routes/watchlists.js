b39c8cc6292c6f28dc1e7096c3b1bc7619c4cbcd
hiluan <apt.muenchen@gmail.com>
2022-03-29 20:00:50 -0700

    remove tickersWithQuotes

--- routes/watchlists.js
+++ routes/watchlists.js
@@ -40,7 +40,6 @@ watchlistsdRoutes.route("/watchlists/add").post((req, response) => {
     listName: req.body.listName,
     emoji: req.body.emoji,
     tickers: req.body.tickers,
-    tickersWithQuotes: req.body.tickersWithQuotes, // this only for using redux state
   };
   db_connect.collection("watchlists").insertOne(myobj, (err, res) => {
     if (err) throw err;
